c8f07b08b9e7a677f431db60e0010d0f78410fdb
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-10-03 11:55:43 -0700

    Save early save often

--- app.js
+++ app.js
@@ -1,67 +1,93 @@
-"use strict";
+'use strict';
 
-//make an object
-//randomly display one picture
-//listener, sometjomg tl click on
-
+console.log( 'Defining the random function' );
 var random = function(min, max){
   return( Math.round(Math.random() * (max - min) + min));
 };
 
-var itemPool = [];
+console.log( 'Item.pool is where all the created Items will go.' );
+Item.pool = [];
 
-var recentPool1 = [];
+console.log( 'Item.recentPool1 is where Items already displayed currently go.' );
+Item.recentPool1 = [];
 
-var recentPool2 = [];
+console.log( 'Item.recentPool2 is where Items displayed last roll go.' );
+Item.recentPool2 = [];
 
-function item(name,filepath){
-  // check for duplicate name here!
+console.log( 'Item.rolls keeps track of how many times the user has been shown Items.' );
+Item.rolls = 0;
+
+console.log( 'Linking the "slots" elements from the DOM' );
+Item.slot1 = document.getElementById('slot1');
+Item.slot2 = document.getElementById('slot2');
+Item.slot3 = document.getElementById('slot3');
+
+console.log( 'Defining Item constructer.' );
+function Item(name,filepath){
+  console.log( 'Checking' + name + ' against Item.pool to see if it already exists. ' );
+  if(Item.duplicateCheck(name,'Pool')) {
+    console.log('The item ' + name + ' already exists!');
+    die();
+  }
   this.name = name;
   this.filepath = filepath;
   this.seen = 0;
   this.clicked = 0;
-  itemPool.push(this);
+  Item.pool.push(this);
 }
 
-new Image('Bag','resources/bag.jpg');
-new Image('Bananna','resources/bananna.jpg');
-new Image('Bathroom','resources/bathroom.jpg');
-new Image('Bendycharge','resources/bendcharge.jpg');
-new Image('Breakfast','resources/breakfast.jpg');
-new Image('Bubblegum','resources/bubblegum.jpg');
-
-var game = document.getElementById('game');
-var slot1 = document.getElementById('slot1');
-var slot2 = document.getElementById('slot2');
-var slot3 = document.getElementById('slot3');
-
-for(i = 1; i > 0; i--) {
-  // get a random element
-  var itemIndex = random(0,itemPool.length);
-  var item = itemPool[itemIndex];
-  var recentMatch = false;
-  for(n = 0; n < recentPool1.length; n++) {
-    if(item[name] === recentPool1[n]) {
-      recentMatch = true;
-      break;
-    };
-  };
-  if(!recentMatch) {
-    for(n = 0; n < recentPool2.length; n++) {
-      if(item[name] === recentPool2[n]) {
-        recentMatch = true;
-        break;
+Item.prototype.duplicateCheck = function(itemName, pool) {
+  if(pool === 'Pool') {
+    for(n = 0; n < Item.pool.length; n++) {
+      if(itemName === recentPool1[n]) {
+        return(true);
       };
     };
   };
-  if(recentMatch) {
-    console.log(item[name] + 'has been used too recently.');
-  } else {
-    //slot name?
-    slot1.src = itemPool.itemIndex.filepath
+  if (pool === 'recentPool') {
+    for(n = 0; n < Item.recentPool1.length; n++) {
+      if(itemName === recentPool1[n]) {
+        return(true);
+      };
+    };
+    for(n = 0; n < Item.recentPool2.length; n++) {
+      if(itemName === recentPool2[n]) {
+        return(true);
+      };
+    };
+  };
+  return(false);
+};
 
-    break;
+// displayItems function added to Item constructer
+Item.prototype.displayItems = function() {
+  for(i = 3; i > 0; i--) {
+    // get a random element
+    var itemIndex = random(0,Item.pool.length);
+    var item = Item.pool[itemIndex];
+    if(Item.duplicateCheck(name,'recentPool')) {
+      con.log(item[name] + 'has been used too recently.');
+    } else {
+      if(i === 1) {
+        slot1.src = Item.pool.itemIndex.filepath;
+      } else if(i === 2) {
+        slot2.src = Item.pool.itemIndex.filepath;
+      } else if(i === 3) {
+        slot3.src = Item.pool.itemIndex.filepath;
+      } else {
+        console.log('Something went terribly wrong in the "for each slot" loop.');
+      }
+      break;
+    };
   };
 };
 
-//listener should listen for click on any of the three slots, from there I can grab the item name and trigger the frefresh.
+new Item('Bag','resources/bag.jpg');
+new Item('Bananna','resources/bananna.jpg');
+new Item('Bathroom','resources/bathroom.jpg');
+new Item('Bendycharge','resources/bendcharge.jpg');
+new Item('Breakfast','resources/breakfast.jpg');
+new Item('Bubblegum','resources/bubblegum.jpg');
+
+Item.displayItems();
+//listener should listen foItem.poolon any of the three slots, from there I can grab the item name and trigger the frefresh.
